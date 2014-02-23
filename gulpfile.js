e68b20672d7da28ccd5fcbd9c587ffeae923b146
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-02-24 10:32:43 +1100

    Add task to minify the JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
+var gulp = require('gulp'),
+  gutil = require('gulp-util'),
 
-var coffee = require('gulp-coffee');
+  coffee = require('gulp-coffee'),
+  uglify = require('gulp-uglify'),
+  rename = require('gulp-rename');
 
-var paths = {
-  scripts: ['src/*.coffee']
-}
+  paths = {
+    scripts: ['src/*.coffee'],
+    dest: 'dist'
+  };
 
 gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
     .pipe(coffee())
+    .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task('scripts_min', function() {
+  return gulp.src(paths.scripts)
+    .pipe(coffee())
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['scripts']);
+gulp.task('default', ['scripts', 'scripts_min']);
